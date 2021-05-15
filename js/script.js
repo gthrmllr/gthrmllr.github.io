053e95b94f11f711572e66c5743376df9cf16889
gthrmllr <73273320+gthrmllr@users.noreply.github.com>
2021-05-15 17:21:41 +0200

    aaaa

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let val = -100;
+let limite = -500;
+function mouv(){
+
+    let valeur = val + 'vw';
+    document.querySelector(".container").style.transform="translateX("+valeur+")";
+
+if(val>limite){
+    val = val - 100;
+} else {
+     val = 0;
+}
+}
+setInterval(() => {
+    mouv()
+}, 1000);
