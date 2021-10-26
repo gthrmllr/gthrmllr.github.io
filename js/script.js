08f8860d1f2ab37ba37443b8c1bc9dbe7750bc15
gthrmllr <73273320+gthrmllr@users.noreply.github.com>
2021-10-26 17:55:37 +0200

    aaaa
    aaa

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 function menu(){
     document.querySelector("nav").classList.toggle("ferme");
 }
+
+var position = 0;
+var delta = 100;
+var nbImg = document.querySelector(".slider").children.length;
+var timer;
+
+function partir() {
+    timer = setInterval(function () {
+        if (position < nbImg - 1) {
+            position += 1;
+        } else {
+            position = 0;
+        }
+        document.querySelector(".slider").style.transform = 'translate(-' + (position * delta) + 'vw)';
+    }, 1000);
+}
