c5d3ee0b6124d9c88e8d10da48a2f36a7eb18989
gthrmllr <73273320+gthrmllr@users.noreply.github.com>
2021-10-26 17:35:01 +0200

    aa
    aaa

--- WEBECOM/script2.js
+++
@@ -1,7 +0,0 @@
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('anim2'),
-    rederer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'data.json'
-})
---
+++ js/script.js
@@ -0,0 +1,3 @@
+function menu(){
+    document.querySelector("nav").classList.toggle("ferme");
+}
