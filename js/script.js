9b360f7606d7b83e049ae649969b6d1298cdcb82
gthrmllr <73273320+gthrmllr@users.noreply.github.com>
2021-10-28 16:41:04 +0200

    ronpichame
    roncouapozghqezr

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 function menu(){
     document.querySelector("nav").classList.toggle("ferme");
 }
+
+var myFullpage = new fullpage('#main', {
+    anchors: ['firstPage', 'secondPage', '3rdPage'],
+    navigation: true,
+    navigationPosition: 'right',
+
+});
