6ffc9cd972db25434ee7a125486f81e9a6ba5708
gthrmllr <73273320+gthrmllr@users.noreply.github.com>
2021-10-30 16:21:38 +0200

    a
    a

--- js/script.js
+++ js/script.js
@@ -2,9 +2,18 @@ function menu(){
     document.querySelector("nav").classList.toggle("ferme");
 }
 
-var myFullpage = new fullpage('#main', {
-    anchors: ['firstPage', 'secondPage', '3rdPage'],
-    navigation: true,
-    navigationPosition: 'right',
+var position = 0;
+var delta = 100;
+var nbImg = document.querySelector(".slider").children.length;
+var timer;
 
-});
+function partir() {
+    timer = setInterval(function () {
+        if (position < nbImg - 1) {
+            position += 1;
+        } else {
+            position = 0;
+        }
+        document.querySelector(".slider").style.transform = 'translate(-' + (position * delta) + 'vw)';
+    }, 1000);
+}
