9f285193d6ced908ebf4dc0a03ec49c98ae6b26f
gthrmllr <73273320+gthrmllr@users.noreply.github.com>
2021-12-22 23:59:09 +0100

    aa
    a

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-
-document.querySelector(".menu").addEventListener("click", function(){
+document.querySelector(".menu").addEventListener("click", function() {
     document.querySelector("nav").classList.toggle("nav-visible");
+    document.querySelector("body").classList.toggle("body-overflow");
 })
+
+const bulle = document.querySelector('.bulle');
+const apropos = document.querySelector("#apropos>.bulle")
+window.addEventListener('scroll', () => {
+
+        if (window.scrollY > 350) {
+            document.querySelector("#apropos>.bulle").classList.add('bulle-anim');
+            document.querySelector("#apropos>.bulle:nth-child(2)").classList.add('bulle-anim');
+            document.querySelector("#apropos>.bulle:nth-child(3)").classList.add('bulle-anim');
+        };
+
+    }
+
+)
