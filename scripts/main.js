d0ea4d788d843a9cb11ed4a3c1ece22e040d8662
gthrmllr <73273320+gthrmllr@users.noreply.github.com>
2022-10-30 17:20:43 +0100

    a
    a

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,17 @@
 const menu = document.querySelector(".menu");
-const nav = document.querySelector("nav")
-const lien = document.querySelector("nav ul ")
+const nav = document.querySelector("nav");
+const lien = document.querySelector("nav ul ");
+const bb1 = document.querySelector(".barre-blanche1");
+const bb2 = document.querySelector(".barre-blanche2");
+const bb3 = document.querySelector(".barre-blanche3");
 
 
 document.querySelector(".menu").addEventListener("click", function() {
     nav.classList.toggle("nav-open");
+    bb1.classList.toggle("barre-blanche1-toggle");
+    bb2.classList.toggle("barre-blanche2-toggle");
+    bb3.classList.toggle("barre-blanche3-toggle");
+
 })
 
 lien.addEventListener("click", function() {
